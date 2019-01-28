a7fb00422d5f959a8bc0817e6fc64990bcae0780
mac <mac@macbook-pro.local>
2019-01-28 18:37:20 -0200

    Initial Commit

---
+++ SuperSenha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperSenha
+//
+//  Created by mac on 28/01/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
